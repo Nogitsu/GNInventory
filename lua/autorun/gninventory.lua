3fc9e1f483d374276cd155faa6da61a01fdceb37
Nogitsu <nogitsu.9@gmail.com>
2020-02-02 17:04:43 +0100

    Update gninventory.lua

--- lua/autorun/gninventory.lua
+++ lua/autorun/gninventory.lua
@@ -2,4 +2,6 @@ GNInventory = {}
 
 GNLib.RequireFolder( "gninventory" )
 
-GNLib.EnableAddon( "gninventory", "v0.7.2", "v1" )
+if SERVER then
+    GNLib.EnableAddon( "gninventory", "v0.7.2", "v1" )
+end
